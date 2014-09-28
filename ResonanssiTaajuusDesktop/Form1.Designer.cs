4bfb34f9f92054f0869d0ab6830c8210e7e34917
azone11 <anton.nevala@gmail.com>
2014-09-28 13:44:39 +0300

    setup

---
+++ ResonanssiTaajuusDesktop/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ResonanssiTaajuusDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(305, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Tyhjennä";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(104, 105);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(300, 202);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AccessibleName = "freqTrack";
+            this.trackBar1.Location = new System.Drawing.Point(104, 54);
+            this.trackBar1.Maximum = 10000;
+            this.trackBar1.Minimum = 20;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(312, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.TickFrequency = 10;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 20;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 319);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ResonanssiTaajuusDesktop/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ResonanssiTaajuusDesktop
+{
+    public partial class Form1 : Form
+    {
+        //private System.Windows.Forms.TrackBar trackBar1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            trackBar1.ValueChanged += 
+			new System.EventHandler(TrackBar1_ValueChanged);
+		    this.Controls.Add(this.trackBar1);
+            chart1.Series["Series1"].ChartType = 
+                System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            int i = trackBar1.Minimum;
+            int tick = trackBar1.TickFrequency;
+            while(i <= trackBar1.Maximum)
+            {
+                chart1.Series["Series1"].Points.AddXY(i, 0);
+                i = i + tick;
+            }
+        }
+
+
+        // Handle the TrackBar.ValueChanged event by calculating a value for 
+        // TextBox1 based on the TrackBar value.   
+        private void TrackBar1_ValueChanged(object sender, System.EventArgs e)
+        {
+            Int16 freqVal = (Int16)trackBar1.Value;
+            double currentI = calculate_current((double)freqVal);
+            //TextBox1.Text = (System.Math.Round(TrackBar1.Value / 10.0)).ToString();
+            chart1.Series["Series1"].Points.AddXY(freqVal, currentI);
+            textBox1.Text = "" + freqVal;
+            textBox2.Text = "" + currentI;
+        }
+
+        private double calculate_current(double freq)
+        {
+            double currentI = 0;
+            double voltU = 20;
+            double resR = 40;
+            decimal capC = 5m / 1000000m;
+            double indL = 0.002;
+            double omega = 2 * Math.PI * freq;
+
+            currentI = voltU / Math.Sqrt(
+                Math.Pow(resR, 2) + 
+                Math.Pow(
+                (
+                (omega * indL) -
+                (1 / (double)((decimal)omega * capC)))
+                , 2));
+            return currentI;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void draw_graph(Int16 selectedValue)
+        {
+            chart1.Series["Series1"].Points.AddXY("test", selectedValue);
+        }
+    }
+}
